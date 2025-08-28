d4b9cbe189d3fc2c9df2fc970b56da8eb57b37ec
Jordan Lei <thunderflash.lei@gmail.com>
2025-08-28 12:30:33 -0400

    Remove simulation generation and fitting scripts; deleted sim_generate_and_fit.py and sim_generate_and_fit.sh to streamline the analysis workflow. Updated sim.sh to call batch_fit.sh instead.

---
+++ analysis/scripts/sim.py
@@ -0,0 +1,85 @@
+import sys, os
+import json
+
+#this is a comment
+currentdir = os.getcwd()
+parentdir = os.path.dirname(currentdir) + "/src/"
+sys.path.insert(0, parentdir) 
+from modeling import Model, MultiStart, filter_depth, value_path
+from modelchecking import *
+from utils import NpEncoder, format_games, get_conditions
+import numpy as np
+import tqdm
+
+def check_jobid():
+    if os.getenv('SLURM_ARRAY_TASK_ID') is not None:
+        jobid = int(os.getenv('SLURM_ARRAY_TASK_ID'))
+        print("Job ID\t", jobid)
+        return jobid
+    else:
+        print("No SLURM job ID found")
+        return None
+
+jobid = check_jobid()
+type_ = "R"
+n_repeats = 1
+n_participants = 20
+
+datafile = f"../data/raw/data_{type_}.json"
+print("data file\t", datafile)
+with open(datafile) as f:
+    data = json.load(f)
+
+users = [f"user{i}" for i in range(n_participants)]
+if jobid is not None:
+    users = [users[jobid]]
+
+for user in users:
+    sim_user = f"sim_{user}"
+    model = Model(filter_depth, value_path, variant=type_)
+
+    sim_params = {
+            "model": model.name,
+            "inv_temp": np.random.uniform(-4, 4),
+            "filter_params": {p: np.random.choice(8) for p in get_conditions(type_)}
+    }
+
+    games = format_games(data[user]["data"])
+    gamedata = []
+
+    for _ in tqdm.tqdm(range(n_repeats), total=n_repeats, desc=f"simulating {user}"): 
+        prediction = model.predict(sim_params, games)
+        gamedata.extend([
+                {
+                    "name": f"game_{user}_c{condition}_g{game}",
+                    "p": get_conditions(type_)[condition],
+                    "boards": prediction.boards.isel(conditions=condition, games=game).values,
+                    "oracle": prediction.oracles.isel(conditions=condition, games=game, trials=0).values,
+                    "tuplepath": prediction.paths.isel(conditions=condition, games=game).values,
+                    "path": [f'{a},{b}' for a, b in prediction.paths.isel(conditions=condition, games=game).values],
+                    "actions": prediction.choose_left.isel(conditions=condition, games=game).astype(bool).values,
+                    "is_transition": prediction.is_transition.isel(conditions=condition, games=game).astype(bool).values,
+                    "trials": [{"rt": 0} for _ in range(7)],
+                }
+                for condition in range(5) for game in range(5)
+        ])
+
+    multistart = MultiStart(model, games, {"inv_temp": 5}, use_grid = False, n=100)
+    multistart.sweep()
+
+    filedir = f"../data/sim/fit/{type_}_{model.name}"
+    os.makedirs(filedir, exist_ok=True)
+
+    with open(f"{filedir}/{sim_user}_sim.json", "w") as f:
+        json.dump(sim_params, f, cls=NpEncoder)
+
+    with open(f"{filedir}/{sim_user}_fit.json", "w") as f:
+        json.dump(multistart.best, f, cls=NpEncoder)
+    
+    
+    
+
+
+
+
+
--- analysis/scripts/sim_fit.sh
+++ analysis/scripts/sim_fit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Example of running python script with a job array
-#SBATCH -J sim_gen_fit
+#SBATCH -J model_fit
 #SBATCH -c 1 # one CPU core per task
-#SBATCH -t 4:00:00
+#SBATCH -t 3:00:00
 #SBATCH --mem=4GB
 #SBATCH --array=0-20
 #SBATCH -o log/%j_%a.out
 #SBATCH -e log/%j_%a.error 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hl3976@nyu.edu  
-#SBATCH --job-name=TH-sim-gen-fit
+#SBATCH --job-name=TH-Final-Fit
 
 # activate the venv
 source /scratch/hl3976/ma_lab/.venv/bin/activate
 
 # run the python script
-python sim_generate_and_fit.py --type=$1
+python sim.py --type=$1
