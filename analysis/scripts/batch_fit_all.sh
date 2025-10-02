6c5cfc39de7c84d1e4c7c6739aad90bbd6a3a3c6
Jordan Lei <thunderflash.lei@gmail.com>
2025-10-02 16:06:54 -0400

    Updating batch fit code

--- analysis/scripts/batch_fit_all.sh
+++ analysis/scripts/batch_fit_all.sh
@@ -3,9 +3,9 @@ rm -rf log/*
 sbatch batch_fit_all_array.sh R raw
 sbatch batch_fit_all_array.sh T raw
 sbatch batch_fit_all_array.sh V raw
-sbatch batch_fit_all_array.sh R sim_variable_depth
-sbatch batch_fit_all_array.sh T sim_variable_depth
-sbatch batch_fit_all_array.sh V sim_variable_depth
-sbatch batch_fit_all_array.sh R sim_variable_inv_temp
-sbatch batch_fit_all_array.sh T sim_variable_inv_temp
-sbatch batch_fit_all_array.sh V sim_variable_inv_temp
+sbatch batch_fit_all_array.sh R simulated_filteradapt
+sbatch batch_fit_all_array.sh T simulated_filteradapt
+sbatch batch_fit_all_array.sh V simulated_filteradapt
+sbatch batch_fit_all_array.sh R simulated_policycomp
+sbatch batch_fit_all_array.sh T simulated_policycomp
+sbatch batch_fit_all_array.sh V simulated_policycomp
