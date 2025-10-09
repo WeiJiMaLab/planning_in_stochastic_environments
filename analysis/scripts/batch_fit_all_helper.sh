64ba82242655519b1ef0e20e8bd94fa81ddcc5b8
Jordan Lei <thunderflash.lei@gmail.com>
2025-10-09 12:10:12 -0400

    Changing batch parameters

--- analysis/scripts/batch_fit_all_helper.sh
+++ analysis/scripts/batch_fit_all_helper.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Example of running python script with a job array
-#SBATCH -J model_fit
+#SBATCH -J model_fit_${1}_${2}
 #SBATCH -c 1 # one CPU core per task
-#SBATCH -t 3-00:00:00
-#SBATCH --mem=6GB
+#SBATCH -t 2-00:00:00
+#SBATCH --mem-per-cpu=4G
 #SBATCH --array=0-99
-#SBATCH -o log/%j_%a.out
-#SBATCH -e log/%j_%a.error 
+#SBATCH -o log/%j_${1}_${2}_%a.out
+#SBATCH -e log/%j_${1}_${2}_%a.error 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hl3976@nyu.edu  
-#SBATCH --job-name=Fitter
+#SBATCH --job-name=Fit_${1}_${2}
 
 # activate the venv
 source /scratch/hl3976/ma_lab/.venv/bin/activate
